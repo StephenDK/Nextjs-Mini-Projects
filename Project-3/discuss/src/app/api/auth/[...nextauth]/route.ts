ef91a4febad0f4b0dd547032d99a132d5cfd7c62
StephenDK <stephendklein08@gmail.com>
2025-07-12 13:35:32 -0700

    The Theory Behind OAuth

---
+++ Project-3/discuss/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,10 @@
+// We would create this folder structure if outside servers need to talk to our application
+// export async function GET() {
+
+// }
+
+// export async function POST() {
+
+// }
+
+export { GET, POST } from "@/auth";
---
+++ Project-3/discuss/src/auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/db";
+
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw new Error("Missing github oauth credentials");
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signOut,
+  signIn,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Github({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    // Ususally not needed, we are using this to fix a bug in the current working version of nextauth
+    async session({ session, user }: any) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
